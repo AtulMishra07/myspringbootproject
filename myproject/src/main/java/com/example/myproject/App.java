b0fbeb2d68db80880984c6d2561dae557b499ae6
Atul Mishra <Atul Mishra@DESKTOP-1IBU0RJ>
2019-02-24 23:39:57 +0530

    package update

--- myproject/src/main/java/com/example/myproject/App.java
+++ myproject/src/main/java/com/example/myproject/App.java
@@ -1,17 +1,31 @@
 package com.example.myproject;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.example.entity.CreditRating;
+import com.example.service.ICreditRatingService;
 
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-@ComponentScan(basePackageClasses = CreditRatingController.class)
+@SpringBootApplication(scanBasePackages = {"/myproject/src/main/java/com/example/controller"})
 public class App 
 {
 	
@@ -19,6 +33,8 @@ public class App
 	String home() {
 		return "Hello World : Successful Deployment";
 	}
+
+	
     public static void main( String[] args )
     {
         SpringApplication.run(App.class, args);
